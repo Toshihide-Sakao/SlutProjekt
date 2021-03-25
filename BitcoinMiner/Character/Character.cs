5d2f85fee08fbde05283c780b0dc2cc025d81f6a
Toshihide-Sakao <70141444+Toshihide-Sakao@users.noreply.github.com>
2021-03-25 20:10:20 +0900

    Not Much Change

--- BitcoinMiner/Character/Character.cs
+++ BitcoinMiner/Character/Character.cs
@@ -3,20 +3,11 @@ using BitcoinMiner.GameEngine;
 
 namespace BitcoinMiner.Character
 {
-    public class Character : GameObject
+    public abstract class Character : GameObject
     {
         protected string name;
         protected int HP;
 
-        public void Update() 
-        {
-            
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public abstract void Update();
     }
 }
